6bdc517eedb88e18b8c0ce8e1215831de9e2afdd
juandavdidtejedormedina-rgb <juandavdidtejedormedina@gmail.com>
2026-04-16 14:57:50 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import pandas as pd
 import plotly.graph_objects as go
 import requests
+import streamlit as st
 
 # ============================================================================
 # CONFIGURATION AND CONSTANTS
