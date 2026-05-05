3d72ca16b4e753c11d10c90df558c8d0c4bb0108
Ing_Juan-Tejedor <juandavdidtejedormedina@gmail.com>
2026-05-05 12:04:30 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -8881,4 +8881,3 @@ with tab_correlacion:
                 st.info("No hay registros de cortinas para los filtros seleccionados.")
             else:
                 _dataframe(datos_cortinas_sel)
-
